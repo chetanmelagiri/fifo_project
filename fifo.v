85ea97530b937fbd71dd78fa4aec69c5cf8e1fc0
chetanmelagiri <chetan.melagiri@gmail.com>
2019-05-10 18:36:20 +0530

    updated

--- fifo.v
+++ fifo.v
@@ -1,13 +1,12 @@
 module fifo(clock, resetn,write_enb,read_enb,full,empty,data_in, data_out);
 	parameter WIDTH=8;
 	parameter DEPTH=16;
-	parameter PT_WIDTH=WIDTH>>1;
+	parameter PT_WIDTH=$clog2(DEPTH) ;
 	input clock,resetn, write_enb, read_enb;
 	input [(WIDTH-1):0]data_in;
 	output  full, empty;
 	output reg [(WIDTH-1):0]data_out;
 	integer i;
-	reg [6:0]count;
 	
 	reg [PT_WIDTH:0]wr_ptr,rd_ptr;
 
@@ -28,6 +27,9 @@ module fifo(clock, resetn,write_enb,read_enb,full,empty,data_in, data_out);
 		else if (write_enb && ~full)begin
 			mem[wr_ptr[(PT_WIDTH-1):0]] <= data_in;
 		end
+        	else if(full)begin
+            	$display("ERROR:FIFO IS FULL; CAN'T WRITE");
+        	end
 	end
 
 	//	address generator
@@ -45,16 +47,23 @@ module fifo(clock, resetn,write_enb,read_enb,full,empty,data_in, data_out);
 				 rd_ptr <= rd_ptr+1;
 			end
 		end
-	end	
-
+	end
+	
+    //Read logic
+    //Data is read when read_enb is asserted high and empty is asserted low.
 	always@(posedge clock)begin	
 		if(~resetn)begin
 			data_out <= DEPTH'b0;
 		end					
 		else if (read_enb && ~empty)begin
 			data_out <= mem[rd_ptr[(PT_WIDTH-1):0]];
-		end										
-	end	
+		end
+		else if(empty) begin
+            		$display("ERROR:FIFO IS EMPTY; NOTHING TO READ");
+			data_out<= 'hz;
+		end
+	end
+			
 
 
 endmodule
